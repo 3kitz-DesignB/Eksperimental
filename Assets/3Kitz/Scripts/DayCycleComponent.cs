4da8068608374b081c88575b519b424978ea8c86
CptFinn <anders.pw@hotmail.com>
2018-05-02 21:32:23 +0200

    Revert "Revert "Add ECS DayCycle system""
    This reverts commit 70859b0ab9a1af363bbb65304f1b788dd1ea84fe.

---
+++ Assets/3Kitz/Scripts/DayCycleComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct DayCycle : IComponentData
+{
+    public float Speed;
+}
+
+public class DayCycleComponent : ComponentDataWrapper<DayCycle>
+{
+}
